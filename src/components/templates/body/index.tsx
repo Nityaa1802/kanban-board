010ffc723efdcece1c523b678e8f5bf8d9bf1865
NityaAgarwal18 <nitya.agarwal_cs.aiml20@gla.ac.in>
2023-11-19 12:32:30 +0530

    designed the body of the dashboard

---
+++ src/components/templates/body/index.tsx
@@ -0,0 +1,44 @@
+import { useContext } from 'react';
+
+import './index.css';
+import { TicketContext } from '../../../context/tickets';
+import Rows from '../../organism/rows';
+import Categories from '../../molecules/catagories';
+import { getIcon, priority, status } from '../../../util/constants';
+export default function Body() {
+  const { groupedTickets, grouping, users } = useContext(TicketContext);
+  function getHeader() {
+    switch (grouping) {
+      case 'status':
+        return status;
+      case 'priority':
+        return priority;
+      case 'users':
+        return users;
+      default:
+        return [];
+    }
+  }
+  return (
+    <div id='body'>
+      {getHeader().map((item: any) => {
+        return (
+          <div id='row' key={item.name}>
+            <Categories
+              key={item.name}
+              {...(grouping !== 'users' && { Icon: getIcon.get(item.name) })}
+              item={item}
+            />
+            <Rows
+              tickets={
+                grouping === 'status'
+                  ? groupedTickets[item.name]
+                  : groupedTickets[item.id]
+              }
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
