69d715b52fd57a753085a7b790f76b49877760bf
NityaAgarwal18 <nitya.agarwal_cs.aiml20@gla.ac.in>
2023-11-19 17:11:48 +0530

    added a new icon in button

--- src/components/atom/Button/index.tsx
+++ src/components/atom/Button/index.tsx
@@ -1,3 +1,4 @@
+import { ReactComponent as ChevronUp } from '../../../assets/icons/chevron-up.svg';
 import { ReactComponent as ChevronDown } from '../../../assets/icons/chevron-down.svg';
 import './index.css';
 
@@ -5,16 +6,27 @@ export default function Button({
   onClick,
   Icon,
   placeHolder,
+  open,
 }: {
-  onClick:  any;
+  onClick: any;
   Icon?: any;
   placeHolder: string;
+  open: boolean;
 }) {
   return (
-    <button className='button' onClick={onClick}>
+    <button
+      className='button'
+      onClick={(event) => {
+        onClick(event);
+      }}
+    >
       {Icon && <Icon />}
-      <p>{placeHolder}</p>
-      <ChevronDown className='chevron' />
+      <p className='label'>{placeHolder}</p>
+      {open ? (
+        <ChevronUp className='chevron' />
+      ) : (
+        <ChevronDown className='chevron' />
+      )}
     </button>
   );
 }
